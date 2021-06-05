c5c6b0ee34599e31ddf84825cf1a9f0512426b7c
Mike <nortonstudios@gmail.com>
2021-06-04 23:00:52 -0600

    More list method buildout

--- modules/do-list.js
+++ modules/do-list.js
@@ -22,12 +22,58 @@ module.exports = class toDoList {
         return this.tasks.length;
     }
 
+    // Returns task based on index. 
+    getTask(index){
+        if(index < 0 || index > this.tasks.length - 1){
+            throw new Error('Index out of bounds');
+        }
+        if(/*!index ||*/ typeof index !== 'number'){
+            throw new Error('Index not a number');
+        }
+        return this.tasks[index];
+    }
+
+    // Searches for task based on title.
+    getTaskByTitle(title){
+        this.titleCheck(title);
+        const index = this.getIndexByTitle(title);
+        if (index < 0){
+            return this.newItem('Task not found');
+        }
+        return this.getTask(index);
+    }
+
+    //Serches for index by title. Returns index or -1 for 'not found'.
+    getIndexByTitle(title){
+        for (let i = 0; i < this.tasks.length; i++) {
+            const currentTask = this.tasks[i];
+            if(title.toLowerCase() == currentTask.getTitle().toLowerCase()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    titleCheck(title){
+        if( !title || typeof title !== 'string'){
+            throw new Error('Invalid title.');
+        }
+    }
+
+    getList(){
+        return this.tasks;
+    }
+
 
   }
 
 
   //Ideas
   // sort by prioriy, build array with priority value as indexes
+
+  // return to do list between indexes? top ten list?
+
+  // return to do list for a day, week, hour.
 
   // 
 
--- test/do-list.test.js
+++ test/do-list.test.js
@@ -88,9 +88,114 @@ describe('List class', ()=>{
             const actual = lengthTestList.getNumber();
             //assert
             assert.strictEqual(actual, expected);
-
         });
     });
+    describe('List of tasks with one named task', function(){
+        // One name in list 
+        let oneNameTestList = new toDoList();
+        const expected = 'New Task';
+        oneNameTestList.newTask(expected);
+                
+        // create and return named task based on index.
+        it('should make and add named task.', function(){
+            //arrange 
+            //act
+            const actual = oneNameTestList.getTask(0).getTitle();
+            //assert
+            assert.equal(actual, expected);
+        });
+        //Thows if getTask() tries to access out of array bounds
+        it('should throw if out of bounds', function(){
+            //arrange
+            //act
+            //assert
+            assert.throws(function(){oneNameTestList.getTask(1)}, Error);
+        });
+        //Thows if getTask() if parameter is not a number
+        it('should throw if not a number', function(){
+            //arrange
+            //act
+            //assert
+            assert.throws(function(){oneNameTestList.getTask('a')}, Error);
+        });
+        //Thows if getTask() has no parameter
+        it('should throw if no parameter', function(){
+            //arrange
+            //act
+            //assert
+            assert.throws(function(){oneNameTestList.getTask()}, Error);
+         
+        });
+        // Returns highest priority task based of a title
+        it('should return task based on name', function(){
+            //arrange, act
+            const actual = oneNameTestList.getTaskByTitle(expected).getTitle();
+            //assert
+            assert.equal(actual, expected);
+        });
+        // Returns highest priority task based of a title
+        it('should return error message task object if task not found', function(){
+            //arrange, act
+            const actual = oneNameTestList.getTaskByTitle('hotdog').getTitle();
+            //assert
+            assert.equal(actual, 'Task not found');
+        });
+        // Throw for bad title
+        it('should throw if title is not a string or not passed', function(){
+            //arrange, act
+            //assert
+            assert.throws(function(){oneNameTestList.getTaskByTitle()}, Error);
+            assert.throws(function(){oneNameTestList.getTaskByTitle(4)}, Error);
+        });
+    });
+
+    describe('list with several tasks.', function(){
+        // Some names and some default titles in list 
+        let testList = new toDoList();
+        const expected = 'New Task';
+        testList.newTask(expected);
+        testList.newTask();
+        testList.newTask(expected);
+        testList.newTask('Mow lawn');
+
+        //Return task by name from list with many tasks
+        it('should return task based on name', function(){
+            //arrange, act
+            const actual = testList.getTaskByTitle(expected).getTitle().toLowerCase();
+            //assert
+            assert.notStrictEqual(actual, expected.toLocaleUpperCase());
+            assert.notStrictEqual(actual, 'new task'.toLocaleUpperCase());
+            assert.notStrictEqual(actual, 'untitled task'.toLowerCase());
+        });
+
+        // Returns highest priority task based on a title
+        it('should return task based on name', function(){
+            //arrange, act
+            const actual = testList.getTaskByTitle(expected).getTitle();
+            //assert
+            assert.equal(actual, expected);
+        });
+
+        // Returns the entire array as list.
+        it('should return the to-do list', function(){
+            //arrange, act
+            //assert
+            assert.notStrictEqual(testList.getList(), testList);
+            
+        });
+
+
+        it('should delete an item by index', function(){
+            //arrange
+            //act
+            testList.delete(0);
+            //assert
+            assert.notStrictEqual(testList.getList(), testList);
+        })
+    });
+
+    // Delete an item
+
 
 
 })
